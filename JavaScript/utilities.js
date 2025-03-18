a3d348a7005009f3f900ae2cdb01cd3e8607f035
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-18 15:04:39 +0600

    change title

--- JavaScript/utilities.js
+++
@@ -1,4 +0,0 @@
-// const balanceBtn = document.getElementById("available-balance");
-// const donateBtn = document.getElementById("btn-donate");
-// const inputAmount = document.getElementById("input-amount");
-// const updateBalance = document.getElementById("donated-amount");
